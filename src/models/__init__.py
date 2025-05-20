39cd90dfad66d85e2a35e5d5bfc98891bff406ee
enricoguerriero <enrico.guerriero@gmail.com>
2025-05-20 16:03:34 +0200

    prova

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,6 @@
 from .llavanext import LlavaNext
 from .llavanext34 import LlavaNext34
-from .videollava import VideoLLaVA
+from .videollava import VideoLlava
 from .smolvlm import SmolVLM
 from .smolvlm256 import SmolVLM256
 from .timesformer import TimeSformer
