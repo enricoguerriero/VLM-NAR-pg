f59a44c057327708f6c0252b114e2ee1976ee29b
enricoguerriero <enrico.guerriero@gmail.com>
2025-05-26 11:31:12 +0200

    .

---
+++ scripts/few_shot_testing.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+#SBATCH --time=48:00:00
+#SBATCH --job-name=few_shot_testing
+#SBATCH --output=outputs/few_shot_testing.out
+
+# Activate the user environment (uenv)
+uenv verbose cuda-12.2.0 cudnn-12.x-8.8.0
+uenv miniconda3-py39
+
+# Activate the Conda environment
+source activate NewbornEnv
+
+echo "virtual environment activated"
+
+export PYTHONPATH=./
+python src/evaluation/few_shot_testing.py 
+
+
+echo "--- THE END ---"
